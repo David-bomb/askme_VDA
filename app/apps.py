161d10f96dff3cdc398f4387ba20074fa5fa8df3
David <kot.shubin@mail.ru>
2025-03-30 22:39:43 +0300

    Django structure changes

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AskmeVdaConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "askme_VDA"
+    name = "app"
